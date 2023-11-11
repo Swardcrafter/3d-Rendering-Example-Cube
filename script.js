aeabf6f3df7b9a6f274b3c5fb843ae16096ea447
PlotTwist <SaturnWillow@gmail.com>
2023-11-10 22:56:19 -0800

    yahhhh (no ws anymore)

--- script.js
+++ script.js
@@ -1,57 +1,12 @@
 /* wss://3d-render-server.saturnwillow.repl.co/echo */
-
-const main_div = document.getElementById('main');
-const setup_div = document.getElementById('setup');
-main_div.style.display = 'none';
 let render_objects = [];
 
-const ws = new WebSocket("wss://3d-render-server.saturnwillow.repl.co/echo");
 
 
-ws.addEventListener("open", () => {
-    ws.send(JSON.stringify({
-        type: 'init',
-        client_type: 'web'
-    }));
-});
-
-
-
-ws.addEventListener("message", msg => {
-    data = JSON.parse(msg.data);
-	console.log(`Server Sent: ${JSON.stringify(data)}`);
-
-    if(data.type == 'pageLoaded') {
-        main(data.page);
-    }
-});
-
-function loadPage() {
-    const input = document.getElementById('pageId_input');
-    ws.send(JSON.stringify({
-        type: 'loadPageWeb', 
-        page: input.value,
-        client_type: 'web'
-    }));
-}
-
-function main(page) {
-    main_div.style.display = 'block';
-    setup_div.style.display = 'none';
-
-    render_objects = [];
-    
-
-    // Set the interval to achieve 60fps
-    setInterval(render, 1000 / 60); // 1000ms divided by 60fps gives the interval in milliseconds
-
-}
-
-function render() {
-    const background_color = '#cccccc';
-
+function main() {
     const canvas = document.getElementById("main-canvas");
     const ctx = canvas.getContext("2d");
+    const background_color = '#cccccc';
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -63,8 +18,23 @@ function render() {
     ctx.fillStyle = background_color;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    render_objects = [];
+    
+
+    // Set the interval to achieve 60fps
+    setInterval(render, 1000 / 60); // 1000ms divided by 60fps gives the interval in milliseconds
+}
+
+function render() {
+    const background_color = '#cccccc';
+
+    const canvas = document.getElementById("main-canvas");
+    const ctx = canvas.getContext("2d");
+
     // Clear the canvas on each frame
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
 
     // Render the background
     ctx.fillStyle = background_color;
@@ -74,3 +44,43 @@ function render() {
         object.Render();
     }
 }
+
+document.addEventListener('keydown', (event) => {
+    if(event.key == 'j') {
+        const canvas = document.getElementById("main-canvas");
+        const ctx = canvas.getContext("2d");
+
+        const object = prompt('Object?');
+        let object_center = prompt('Object center?');
+        let object_size = prompt('Object size?');
+        let object_dotStyle = prompt('Object dot style?');
+
+        // new Cube([500, 500, 500], 100, 'circ')
+
+        const obj_class = eval(object);
+
+        if(object_center == '') {
+            object_center = [canvas.width/2, canvas.height/2, -100];
+        } else {
+            object_center = object_center.split(', ').map(coord => parseFloat(coord));
+        }
+
+        if(object_dotStyle == '') {
+            object_dotStyle = 'circ';
+        }
+
+        if(object_size == '') {
+            object_size = 100;
+        } else {
+            object_size = parseFloat(object_size);
+        }
+
+        console.log(object_size);
+
+        const new_obj = new obj_class(object_center, object_size, object_dotStyle);
+
+        render_objects.push(new_obj);
+    }
+})
+
+main();
