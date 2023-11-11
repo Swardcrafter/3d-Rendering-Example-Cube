b2b15a3d076c78fb34e8634a18f8ef240d9b73de
PlotTwist <SaturnWillow@gmail.com>
2023-11-10 21:38:06 -0800

    1

--- script.js
+++ script.js
@@ -1,23 +1,68 @@
-const background_color = '#cccccc';
+/* wss://3d-render-server.saturnwillow.repl.co/echo */
 
-var canvas = document.getElementById("main-canvas");
-var ctx = canvas.getContext("2d");
+const main_div = document.getElementById('main');
+const setup_div = document.getElementById('setup');
+main_div.style.display = 'none';
+let render_objects = [];
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-// Set 0, 0
-ctx.translate(0, canvas.height);
-ctx.scale(1, -1);
-
-ctx.fillStyle = background_color;
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+const ws = new WebSocket("wss://3d-render-server.saturnwillow.repl.co/echo");
 
 
-// const pyramid = new Pyramid([canvas.width/2, canvas.height/2, 0], 100, 'circ');
-const cube = new Cube([canvas.width/2, canvas.height/2, 0], 100, 'circ');
+ws.addEventListener("open", () => {
+    ws.send(JSON.stringify({
+        type: 'init',
+        client_type: 'web'
+    }));
+});
+
+
+
+ws.addEventListener("message", msg => {
+    data = JSON.parse(msg.data);
+	console.log(`Server Sent: ${JSON.stringify(data)}`);
+
+    if(data.type == 'pageLoaded') {
+        main(data.page);
+    }
+});
+
+function loadPage() {
+    const input = document.getElementById('pageId_input');
+    ws.send(JSON.stringify({
+        type: 'loadPageWeb', 
+        page: input.value,
+        client_type: 'web'
+    }));
+}
+
+function main(page) {
+    main_div.style.display = 'block';
+    setup_div.style.display = 'none';
+
+    render_objects = [];
+    
+
+    // Set the interval to achieve 60fps
+    setInterval(render, 1000 / 60); // 1000ms divided by 60fps gives the interval in milliseconds
+
+}
 
 function render() {
+    const background_color = '#cccccc';
+
+    const canvas = document.getElementById("main-canvas");
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    // Set 0, 0
+    ctx.translate(0, canvas.height);
+    ctx.scale(1, -1);
+
+    ctx.fillStyle = background_color;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
     // Clear the canvas on each frame
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -25,8 +70,7 @@ function render() {
     ctx.fillStyle = background_color;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    cube.Render();
+    for(object of render_objects) {
+        object.Render();
+    }
 }
-
-// Set the interval to achieve 60fps
-setInterval(render, 1000 / 60); // 1000ms divided by 60fps gives the interval in milliseconds
